1150fcd34eb560cf220bf5252f14280d64289e8f
gevogle <voglergeorgina@gmail.com>
2026-06-12 08:44:29 +0000

    Agregar pagina 404

--- app.py
+++ app.py
@@ -19,3 +19,7 @@ def servicios():
 @app.route("/contacto")
 def contacto():
     return render_template("base.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
